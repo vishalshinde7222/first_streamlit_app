e3edac1d087ecbbcdb2d407638d3c255c21c4a7e
Vishal shinde <119867166+vishalshinde7222@users.noreply.github.com>
2022-12-06 11:26:48 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -9,4 +9,4 @@ streamlit.header('Build Your Own Fruit Smoothie')
 
 import pandas
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.datafram(my_fruit_list)
+streamlit.dataframe(my_fruit_list)
