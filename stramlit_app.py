e37134ed5ebd9b10db58e8fb75817cf7ff9308bf
Vishal shinde <119867166+vishalshinde7222@users.noreply.github.com>
2022-12-06 11:21:36 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -1,6 +1,7 @@
 import streamlit;
 streamlit.title('My Parents New Healthy Dinner ')
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Eggs')
+streanlit.header('Build Your Own Fruit Smoothie')
