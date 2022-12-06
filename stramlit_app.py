c4484ca15c7c1cd940936ac7d9909b929fff1c39
Vishal shinde <119867166+vishalshinde7222@users.noreply.github.com>
2022-12-06 11:24:24 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -5,3 +5,8 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Eggs')
 streamlit.header('Build Your Own Fruit Smoothie')
+
+
+import pandas
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+sreamlit.datafram(my_fruit_list)
