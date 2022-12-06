b75b34c99ff020210056cec980ce3629289768d3
Vishal shinde <119867166+vishalshinde7222@users.noreply.github.com>
2022-12-06 12:07:15 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -1,13 +1,17 @@
 import streamlit;
+import pandas;
 streamlit.title('My Parents New Healthy Dinner ')
+streamlit.header('🥑 November 30th')
+streamlit.text('🐔 Michigan')
+streamlit.text('🥗 Illinois')
+streamlit.title('🍞 My Streamlit App')
 streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Eggs')
-streamlit.header('Build Your Own Fruit Smoothie')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
-import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
