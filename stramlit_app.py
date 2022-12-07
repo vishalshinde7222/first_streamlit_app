21a1c5d9584a87588aef2bc731a88ac101acb0f8
Vishal shinde <119867166+vishalshinde7222@users.noreply.github.com>
2022-12-08 00:38:50 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -12,6 +12,8 @@ streamlit.text('🥗 Illinois')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['avacado','strawberries'])
+
+#my_fruit_list = my_fruit_list.set_index('Fruit')
