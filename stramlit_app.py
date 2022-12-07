5a3de1abb36a93c9cdb67266cf299c4e825a2146
Vishal shinde <119867166+vishalshinde7222@users.noreply.github.com>
2022-12-08 00:56:09 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -31,7 +31,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_ch
 # write your own comment -what does the next line do? 
 #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #streamlit.dataframe(fruityvice_normalized)
-#streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response)
 
 
 
